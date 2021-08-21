5c859277b7b952a1e4aa1d1a64405b043a3cb7cb
Banuka Ambegoda <89307136+BanukaKA@users.noreply.github.com>
2021-08-22 02:32:56 +0530

    Add files via upload

---
+++ LCD_code/LCD_code.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h> 
+int Contrast=75;
+ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
+
+ void setup()
+ {
+    analogWrite(6,Contrast);
+     lcd.begin(16, 2);
+  } 
+     void loop()
+ { 
+     lcd.setCursor(0, 0);
+     lcd.autoscroll();
+     lcd.print("#BKA#");
+     delay(500);
+   
+    lcd.setCursor(0, 1);
+    lcd.autoscroll();
+     lcd.print(" SLQ");
+     delay(500);
+
+     
+ }
